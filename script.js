741334904cb2d6df671e3447712e640b4d622234
wasim475 <wasimhossain575@gmail.com>
2023-09-19 00:04:10 +0600

    guessing game done

--- script.js
+++ script.js
@@ -6,20 +6,60 @@ let h2 = document.querySelector('h2')
 let h3 = document.querySelector('h3')
 let count = document.querySelector('.count')
 let err = document.querySelector('.err')
+let main = document.querySelector('.main')
+let user = document.querySelector('.user')
+let userName = document.querySelector('.userName')
+let userbtn = document.querySelector('.userbtn')
+let player2name = document.querySelector('.player2name')
+let player2 = document.querySelector('.player2')
+let winner = document.querySelector('.winner')
+let wName = document.querySelector('.wName')
+let lName = document.querySelector('.lName')
+let loser = document.querySelector('.loser')
+let userErr = document.querySelector('.userErr')
+
+
+userbtn.addEventListener('click', function(){
+  if(!userName.value){
+    userErr.innerHTML = 'Empty field.'
+    userName.value = ''
+  }
+  else if((userName.value-11) ){
+    h2.style.display = 'block'
+    userErr.innerHTML = 'Number not allow as a player name.'
+    userName.value = ''
+  }
+  else if(userName.value.length<3){
+    userErr.innerHTML = 'Player name must be at least 3 characters long...'
+    userName.value = ''
+  }
+  
+  else{
+    user.style.display = 'none'
+    main.style.display = 'block'
+    userErr.style.display = 'none'
+    input1.focus()
+  }
+
+})
 
 submit.addEventListener('click',function(){
     if(!input1.value){
         h2.innerHTML = 'Field is empty';
         h2.style.display = 'block';
+      
+
     }
     else if((input1.value-11==0)? false:!(input1.value-11) ){
-        h2.innerHTML = 'String not allow'
+        h2.innerHTML = 'Characters not allow. Enter a number between 1 to 10. '
         h2.style.display = 'block';
+        input1.value = ''
     }
     else if(!(input1.value>0 && input1.value<=10)){
         
-        h2.innerHTML = 'Please inter a number between 1 to 10.'
+        h2.innerHTML = 'Please enter a number between 1 to 10.'
         h2.style.display = 'block';
+        input1.value = ''
     }
     else{
         input1.style.display = 'none';
@@ -28,8 +68,9 @@ submit.addEventListener('click',function(){
         input2.style.display = 'inline'
         h3.style.display = 'block'
         h2.style.display = 'none'
-
-
+        player2name.innerHTML = userName.value
+        player2.style.display = 'block'
+        input2.focus()
     }
 })
 
@@ -42,21 +83,27 @@ check.addEventListener('click',function(){
         input2.value = ''
     }
     else if(input2-20== 0 ? false: !(input2.value-20)){
-        err.innerHTML= 'String not allow'
+        err.innerHTML= 'Try to guess a number between 1 to 10.'
         input2.value = ''
     }
     else if(!(input2.value>0 && input2.value<=10)){
-        err.innerHTML= 'please enter a value between 1 to 10'
+        err.innerHTML= 'The number you have to guess is between 1 to 10.'
         input2.value = ''
     }
     else if(input2.value==input1.value){
         counter --
         count.innerHTML = counter;
-        err.innerHTML = 'Congrats! You win.'
-        err.style.fontSize = '54px'
+        winner.style.display = 'block'
+        wName.innerHTML = userName.value
+        winner.style.fontSize = '54px'
         h3.style.display = 'none'
+        h2.style.display = 'none'
         input2.style.display = 'none'
         check.style.display = 'none'
+        input2.style.display = 'none'
+        player2.style.display = 'none'
+        input2.value = ''
+        userName.value = ''
     }
     else{
         err.style.display = 'none'
@@ -65,12 +112,17 @@ check.addEventListener('click',function(){
         input2.value = ''
         if(counter<1 || input1.value == input2.value){
             h3.style.display = 'none'
-            h2.innerHTML = 'Ops! You lose'
-            h2.style.fontSize= '54px'
-            h2.style.display = 'block'
+            loser.style.display = 'block'
+            lName.innerHTML = userName.value
+            winner.style.fontSize= '54px'
+            h2.style.display = 'none'
             input2.style.display = 'none'
             check.style.display = 'none'
             err.style.display = 'none'
+            input2.style.display = 'none'
+            player2.style.display = 'none'
+            input2.value = ''
+            userName.value = ''
             
         }
     }
